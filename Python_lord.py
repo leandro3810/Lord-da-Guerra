a6f301ce30a727558b18809a036dc549d55a0a8e
leandro3810 <ledielsong9@gmail.com>
2025-01-08 20:33:12 -0300

    2025 08/01/2025 20:33

--- Python_lord.py
+++ Python_lord.py
@@ -7,62 +7,68 @@ from xml.etree.ElementTree import fromstring
 from xml.sax.saxutils import XMLGenerator
 
 "CLASS-PROJECTING" 
-#("Classteste"): (class)XMLGenerator; # type: ignore
- print :(projeto  do Desenho e codigos do programa);
+#("Classteste"): (class)XMLGenerator; # type: ignore:
 
 property ("Meu jogos");
 
 #("Estali meu jogo nesse programa");(J1939_NO_NAME);
-   print:("Jogo para window");
+
 #("Estali meu jogo nesse programa");(J1939_NO_ADDR);
- print:("Desenho"); desenhar_quadrado(5);
+
 #("x = 1 Altura - lagura do quadrado");
-   ElementTree:("y = 1 Altura - Lagura do quadrado"); ElementTree:("x = 1 Altura - Lagura do quadrado");
+
 #("y = 1 Altura - Lagura do quadrado");
-      range:("x = 1 Altura - Lagura do quadrado");range:("y = 1 Altura - Lagura do quadrado");
+ 
 #(J1939_PGN_MAX);   (J1939_PGN_ADDRESS_COMMANDED);
-    print:("Jogo para xbox"); print:("Desenho");
-#(x = 1 , y = 1);("Altura _ Lagura x = 1 j1939_PGN_REQUEST") * ("Altura _ Lagura y = 1 J1939_PGN_ADDRESS_CLAIMED");
-  import: IncrementalNewlineDecoder, IndentedHelpFormatter, J1939_NLA_PAD, J1939_NO_ADDR, J1939_NO_NAME, J1939_NO_PGN, J1939_PGN_ADDRESS_CLAIMED, J1939_PGN_ADDRESS_COMMANDED, J1939_PGN_MAX, J1939_PGN_REQUEST, PROJECTING, xmlbuilder, fromstring, XMLGenerator;
+    
+#(x = 1 , y = 1);("Altura _ Lagura x = 1 j1939_PGN_REQUEST") * ("Altura _ Lagura y = 1 J1939_PGN_ADDRESS_CLAIMED");: IncrementalNewlineDecoder, IndentedHelpFormatter, J1939_NLA_PAD, J1939_NO_ADDR, J1939_NO_NAME, J1939_NO_PGN, J1939_PGN_ADDRESS_CLAIMED, J1939_PGN_ADDRESS_COMMANDED, J1939_PGN_MAX, J1939_PGN_REQUEST, PROJECTING, xmlbuilder, fromstring, XMLGenerator;
+
 #("10 = 10 * (20 = 20) ");
-    y = 10, x = 50;
+
 #("0.1< 2.0");
-  x = 1, y = 2;
+
 #(' 0.1, 2.0   10 = 10 * 20 = 20');
-  x >y;(0.1, 2.0);(10 = 10 * 20 = 20);
+
 #("Do meu jogo");
+
 #("Jogo para window");
+
 #("Desenho") ("x =1 Altura - Lagura do quadrado")("y = 1 Altura - Lagura do quadrado");
+
 # 1/1_2/3 sobre x=1\y=10;
+
 #("figura do desenho") ("x =10 Altura - Lagura do quadrado")("y = 10 Altura - Lagura do quadrado");
+
 #20/20_10/1.25 y=10\x=50;
+
 #("Jogabilidade do desenho") ("x =1 Altura - Lagura do quadrado")("y = 1 Altura - Lagura do quadrado");
+
 # ("J1939_NO_PGN"),("J1939_NLA_PAD")<("j1939_PGN-MAX"),("j1939_PGN_ADDRESS_COMMANDED");
+
 #("Sensibilidade do desenho") ("x =100% Altura - Lagura do quadrado")("y = 100% Altura - Lagura do quadrado");
+
 #x=1<y=2.(altura*lagura);
+
 #tamanho do quadrado 2.00=2.00;
+
 #("Desenho") ("x =1 Altura - Lagura do quadrao")("y = 1 Altura - Lagura do quadrado");("figura do desenho") ("x =10 Altura - Lagura do quadrado")("y = 10 Altura - Lagura do quadrado");
+
 #("jogobilidade do desenho") ("x =1 Altura - Lagura do quadrado")("y =1 Altura - Lagura do quadrado")("sensinilidade do desenho") ("x =100% Altura - lagura do quadrado")("y =100% Altura - Lagura do quadrado");
+
 # figura ,jogabilidade ,sensibilidade;
+
 #x=1<y=2.(altura*lagura);
+
 #tamanho do quadrado 2.00=2.00;
+
 #figura do jogo;
+
 #jogabilidade do jogo;
+
 # figura de bolas(tamanho da figura=(0.25x0.90));
+
 #jogabilidade do jogo para window;
+
 #jogabilidade do jogo para xbox;
+
 #finaliza o projeto de comitim....
-def desenhar_quadrado(tamanho):
-    for i in range(tamanho):
-        print('*' * tamanho)
-  traço = 0.1
-desenhar_quadrado(5),
-desenhar_quadrado(10),type(IncrementalNewlineDecoder),type(IndentedHelpFormatter),type(J1939_NLA_PAD),type(J1939_NO_ADDR),type(J1939_NO_NAME),type(J1939_NO_PGN),type(J1939_PGN_ADDRESS_CLAIMED),type(J1939_PGN_ADDRESS_COMMANDED),type(J1939_PGN_MAX),type(J1939_PGN_REQUEST),type(PROJECTING),type(xmlbuilder),type(fromstring),type(XMLGenerator).
-desenhar_quadrado(15),type(IncrementalNewlineDecoder),type(IndentedHelpFormatter),type(J1939_NLA_PAD),type(J1939_NO_ADDR),type(J1939_NO_NAME),type(J1939_NO_PGN),type(J1939_PGN_ADDRESS_CLAIMED),type(J1939_PGN_ADDRESS_COMMANDED),type(J1939_PGN_MAX),type(J1939_PGN_REQUEST),type(PROJECTING),type(xmlbuilder),type(fromstring),type(XMLGenerator).
-desenhar_quadrado(20),type(IncrementalNewlineDecoder),type(IndentedHelpFormatter),type(J1939_NLA_PAD),type(J1939_NO_ADDR),type(J1939_NO_NAME),type(J1939_NO_PGN),type(J1939_PGN_ADDRESS_CLAIMED),type(J1939_PGN_ADDRESS_COMMANDED),type(J1939_PGN_MAX),type(J1939_PGN_REQUEST),type(PROJECTING),type(xmlbuilder),type(fromstring),type(XMLGenerator).
-def figura_de_bolas(tamanho):
-     import  figura_de_bolas(tamanho):
-        print('*' * tamanho).
-figura_de_bolas(0.25),type(IncrementalNewlineDecoder),type(IndentedHelpFormatter),type(J1939_NLA_PAD),type(J1939_NO_ADDR),type(J1939_NO_NAME),type(J1939_NO_PGN),type(J1939_PGN_ADDRESS_CLAIMED),type(J1939_PGN_ADDRESS_COMMANDED),type(J1939_PGN_MAX),type(J1939_PGN_REQUEST),type(PROJECTING),type(xmlbuilder),type(fromstring),type(XMLGenerator).
-figura_de_bolas(0.90),type(IncrementalNewlineDecoder),type(IndentedHelpFormatter),type(J1939_NLA_PAD),type(J1939_NO_ADDR),type(J1939_NO_NAME),type(J1939_NO_PGN),type(J1939_PGN_ADDRESS_CLAIMED),type(J1939_PGN_ADDRESS_COMMANDED),type(J1939_PGN_MAX),type(J1939_PGN_REQUEST),type(PROJECTING),type(xmlbuilder),type(fromstring),type(XMLGenerator).
-global x = 1, y = 1.
--- README.md
+++ README.md
@@ -23,22 +23,38 @@
 #   ("Do meu jogo");
 
 # ("Desenho") ("x =1 Altura - Lagura do quadrado")("y = 1 Altura - Lagura do quadrado");
+
 # 1/1_2/3 sobre x=1\y=10;
+
 # ("figura do desenho") ("x =10 Altura - Lagura do quadrado")("y = 10 Altura - Lagura do quadrado");
+
 # (20/20_10/1.25 y=10\x=50);
+
 # ("Jogabilidade do desenho") ("x =1 Altura - Lagura do quadrado")("y = 1 Altura - Lagura do quadrado");
+
 # ("J1939_NO_PGN"),("J1939_NLA_PAD")<("j1939_PGN-MAX"),("j1939_PGN_ADDRESS_COMMANDED");
+
 # ("Sensibilidade do desenho") ("x =100% Altura - Lagura do quadrado")("y = 100% Altura - Lagura do quadrado");
+
 # x=1<y=2.(altura*lagura);
+
 # tamanho do quadrado 2.00=2.00;
+
 # ("Desenho") ("x =1 Altura - Lagura do quadrao")("y = 1 Altura - Lagura do quadrado");("figura do desenho") ("x =10 Altura - Lagura do quadrado")("y = 10 Altura - Lagura do quadrado");
+
 # ("jogobilidade do desenho") ("x =1 Altura - Lagura do quadrado")("y =1 Altura - Lagura do quadrado")("sensinilidade do desenho") ("x =100% Altura - lagura do quadrado")("y =100% Altura - Lagura do quadrado");
+
 # figura ,jogabilidade ,sensibilidade;
+
 # x=1<y=2.(altura*lagura);
+
 # tamanho do quadrado 2.00=2.00;
 # figura do jogo;
+
 # jogabilidade do jogo
+
 # figura de bolas (tamanho da figura = (0.25x 0.90));
+
 # configura todas alteração do J1939_PGN_ADDRESS_COMMANDED;
 ],
 }
